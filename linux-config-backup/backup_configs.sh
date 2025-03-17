4c9eac3b180bd8532a57ec8af450a47c137ecd14
André Afonso Ferreira <131478189+andrefonso@users.noreply.github.com>
2025-03-17 11:14:50 -0300

    Update backup_configs.sh
    As linhas referentes ao backup do arquivo de configuração do Samba foram comentadas, para não serem executadas por enquanto no script, de modo que o backup a sere realizado será apenas do arquivo .zshrc.

--- linux-config-backup/backup_configs.sh
+++ linux-config-backup/backup_configs.sh
@@ -2,31 +2,31 @@
 
 # Caminhos dos arquivos de configuração
 zshrc="/home/andre/.zshrc"
-samba_conf="/etc/samba/smb.conf"
+# samba_conf="/etc/samba/smb.conf" - Descomentar essa linha se desejar fazer backup da configuração do Samba
 
 # Diretório de destino para o backup
 backup_dir="/home/andre/mybkp"
 
 # Criar o diretório de backup, se não existir
-mkdir -p "$backup_dir"
+#mkdir -p "$backup_dir"
 
 # Arquivos de backup com timestamp
 timestamp=$(date +'%Y%m%d_%H%M%S')
 zshrc_backup="$backup_dir/.zshrc.bkp_$timestamp"
-samba_conf_backup="$backup_dir/smb.conf.bkp_$timestamp"
+# samba_conf_backup="$backup_dir/smb.conf.bkp_$timestamp" - Descomentar essa linha se desejar fazer backup da configuração do Samba
 
 # Função para copiar arquivos com verificação
-backup_file() {
-    if [ -f "$1" ]; then
-        cp "$1" "$2"
-        echo "Backup de $(basename "$1") concluído: $2"
-    else
-        echo "Arquivo $(basename "$1") não encontrado. Backup não realizado."
-    fi
-}
+#backup_file() {
+    #if [ -f "$1" ]; then
+     #   cp "$1" "$2"
+      #  echo "Backup de $(basename "$1") concluído: $2"
+   # else
+       # echo "Arquivo $(basename "$1") não encontrado. Backup não realizado."
+    #fi
+#}
 
 # Realizar os backups
 backup_file "$zshrc" "$zshrc_backup"
-backup_file "$samba_conf" "$samba_conf_backup"
+#backup_file "$samba_conf" "$samba_conf_backup"
 
 echo "Backup finalizado!"
