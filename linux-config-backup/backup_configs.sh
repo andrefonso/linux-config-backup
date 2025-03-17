5127a28f129f5ab2afcd4ce2ae864b3818507807
André Afonso Ferreira <131478189+andrefonso@users.noreply.github.com>
2025-03-17 11:05:33 -0300

    Create backup_configs.sh
    Criação do diretório de backup e inclusão do script

---
+++ linux-config-backup/backup_configs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Caminhos dos arquivos de configuração
+zshrc="/home/andre/.zshrc"
+samba_conf="/etc/samba/smb.conf"
+
+# Diretório de destino para o backup
+backup_dir="/home/andre/mybkp"
+
+# Criar o diretório de backup, se não existir
+mkdir -p "$backup_dir"
+
+# Arquivos de backup com timestamp
+timestamp=$(date +'%Y%m%d_%H%M%S')
+zshrc_backup="$backup_dir/.zshrc.bkp_$timestamp"
+samba_conf_backup="$backup_dir/smb.conf.bkp_$timestamp"
+
+# Função para copiar arquivos com verificação
+backup_file() {
+    if [ -f "$1" ]; then
+        cp "$1" "$2"
+        echo "Backup de $(basename "$1") concluído: $2"
+    else
+        echo "Arquivo $(basename "$1") não encontrado. Backup não realizado."
+    fi
+}
+
+# Realizar os backups
+backup_file "$zshrc" "$zshrc_backup"
+backup_file "$samba_conf" "$samba_conf_backup"
+
+echo "Backup finalizado!"
